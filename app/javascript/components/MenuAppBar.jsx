afe7ffc908e97985c7502a3ee79a1c5d7810502e
yiyizym <yiyizym@gmail.com>
2018-01-20 14:01:57 +0800

    first init

---
+++ app/javascript/components/MenuAppBar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import AccountCircle from 'material-ui-icons/AccountCircle';
+import Switch from 'material-ui/Switch';
+import { FormControlLabel, FormGroup } from 'material-ui/Form';
+import Menu, { MenuItem } from 'material-ui/Menu';
+
+const styles = {
+  root: {
+    width: '100%',
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class MenuAppBar extends React.Component {
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="contrast" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography type="title" color="inherit" className={classes.flex}>
+              Title
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+MenuAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MenuAppBar);
---
+++ app/javascript/packs/application.jsx
@@ -0,0 +1,32 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+import 'typeface-roboto'
+import Reboot from 'material-ui/Reboot';
+import React from 'react';
+import { render } from 'react-dom';
+import PropTypes from 'prop-types';
+import Grid from 'material-ui/Grid';
+import MenuAppBar from '../components/MenuAppBar';
+
+function App() {
+  return (
+    <div>
+      <Reboot/>
+      <Grid container spacing={0}>
+        <Grid item xs={12}>
+          <MenuAppBar></MenuAppBar>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+document.addEventListener('DOMContentLoaded', () => {
+  render(<App />, document.querySelector('#app'));
+})
